c61fe094ab8c30cf5b6559c3beb3fb8112ab1376
Adam Blomfield <adam@blomfield.me>
2025-01-04 16:59:16 -0600

    cron.php

---
+++ scripts/cron.php
@@ -0,0 +1,41 @@
+<?php
+// Import Settings
+$yamlsettings = yaml_parse_file('/opt/callmigrate/settings.yaml');
+$dbserver = $yamlsettings['Database']['ServerName'];
+$dbuser = $yamlsettings['Database']['Username'];
+$dbpass = $yamlsettings['Database']['Password'];
+$dbname = $yamlsettings['Database']['DBName'];
+
+// Load Settings
+$dbconn = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+if ($dbconn->connect_error) {
+    die("Connection failed: " . $dbconn->connect_error);
+}
+$rssettings = mysqli_query($dbconn, "SELECT * FROM settings") or die("Error in Selecting " . mysqli_error($dbconn));
+$rowsettings = mysqli_fetch_assoc($rssettings);
+$regstatus = $rowsettings["regstatus"];
+$cmserver = $rowsettings["cmserver"];
+
+// Check Registration
+if ($regstatus == 0) {
+  $regurl = "https://" . $cmserver . "/remote/register";
+  $getchreg = curl_init($regurl);
+  curl_setopt($getchreg, CURLOPT_CUSTOMREQUEST, "GET");
+  curl_setopt($getchreg, CURLOPT_RETURNTRANSFER, true);
+  $regjson = curl_exec($getchreg);
+  $regarr = json_decode($regjson);
+  $clientid = $regarr->clientid;
+  $clientsecret = $regarr->clientsecret;
+  $regsql = "UPDATE settings SET regstatus = 1, clientid = '$clientid', clientsecret = '$clientsecret'";
+  $dbconn->query($regsql);
+}
+
+// Check Tasks
+$taskurl = "https://" . $cmserver . "/remote/checktasks";
+$postchtasks = curl_init($taskurl);
+curl_setopt($postchtasks, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($postchtasks, CURLOPT_RETURNTRANSFER, true);
+$taskjson = curl_exec($postchtasks);
+$taskarr = json_decode($taskjson);
+print_r($taskarr);
+?>
