e4bb33e7bf1f41713b794f9c819b162ae0efbacc
Adam Blomfield <adam@blomfield.me>
2025-01-04 17:07:20 -0600

    cron.php

--- scripts/cron.php
+++ scripts/cron.php
@@ -14,6 +14,8 @@ if ($dbconn->connect_error) {
 $rssettings = mysqli_query($dbconn, "SELECT * FROM settings") or die("Error in Selecting " . mysqli_error($dbconn));
 $rowsettings = mysqli_fetch_assoc($rssettings);
 $regstatus = $rowsettings["regstatus"];
+$clientid = $rowsettings["clientid"];
+$clientsecret= $rowsettings["clientsecret"];
 $cmserver = $rowsettings["cmserver"];
 
 // Check Registration
