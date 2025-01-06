2ab6abbe8c7e55101a6518247c5b13f86e598d8f
Adam Blomfield <63602406+ablomfield@users.noreply.github.com>
2025-01-06 09:19:24 -0600

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,3 +1,5 @@
+sudo touch /etc/ssh/ssh_known_hosts
+sudo ssh-keyscan -H callmigrate.click >> /etc/ssh/ssh_known_hosts
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install php php-yaml mysql-server apache2 autossh
