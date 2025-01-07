cc9d717a8c9977fe97d1bb7dab2ad73b7f99a6e6
Adam Blomfield <adam@blomfield.me>
2025-01-07 17:04:36 -0600

    web

---
+++ html/includes/status.php
@@ -0,0 +1,37 @@
+<table>
+    <tr>
+        <td align="right"><b>Registration Status:</b></td>
+        <td><?php echo ($regstatus); ?></td>
+    </tr>
+    <tr>
+        <td align="right"><b>Claim Status:</b></td>
+        <td><?php echo ($claimstatus); ?></td>
+    </tr>
+    <tr>
+        <td align="right"><b>IP Address:</b></td>
+        <td><?php echo (shell_exec("hostname -I")); ?></td>
+    </tr>
+    <tr>
+        <td align="right"><b>Client ID:</b></td>
+        <td><?php echo ($clientid); ?></td>
+    </tr>
+    <tr>
+        <td align="right"><b>Tunnels:</b></td>
+        <td>
+            <?php
+            $rstunnels = mysqli_query($dbconn, "SELECT * FROM tunnels") or die("Error in Selecting " . mysqli_error($dbconn));
+            $tunnelcount = $rstunnels->num_rows;
+            if ($tunnelcount > 0) {
+                while ($tunnelrow = mysqli_fetch_assoc($rstunnels)) {
+                    echo ("                        " . $tunnelrow["tunnelname"] . " (" . $tunnelrow["localhost"] . ":" . $tunnelrow["localport"] . " to " . $tunnelrow["tunnelport"] . ")<br>\n");
+                }
+            } else {
+                echo ("                        None\n");
+            }
+            ?> </td>
+    </tr>
+    <tr>
+        <td align="right"><b>Tunnel Service:</b></td>
+        <td><?php echo (ucwords(shell_exec("systemctl is-active callmigrate-tunnel"))); ?></td>
+    </tr>
+</table>
--- html/index.php
+++ html/index.php
@@ -42,9 +42,16 @@ if ($claimstatus == 1) {
     <link href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
+    <script>
+        var timeout = setInterval(reloadtasks, 5000);
+
+        function reloadstatus() {
+            $('#statusholder').load('/includes/status.php');
+        }
+    </script>
 </head>
 
-<body>
+<body onload="reloadtasks()">
     <div class="parent">
         <div class="cm-logo">
             <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/logo.php"; ?>
@@ -61,43 +68,8 @@ if ($claimstatus == 1) {
         <div class="cm-customer">
         </div>
         <div class="cm-body" style="width: 800px">
-            <table>
-                <tr>
-                    <td align="right"><b>Registration Status:</b></td>
-                    <td><?php echo($regstatus); ?></td>
-                </tr>
-                <tr>
-                    <td align="right"><b>Claim Status:</b></td>
-                    <td><?php echo($claimstatus); ?></td>
-                </tr>                
-                <tr>
-                    <td align="right"><b>IP Address:</b></td>
-                    <td><?php echo(shell_exec("hostname -I")); ?></td>
-                </tr>
-                <tr>
-                    <td align="right"><b>Client ID:</b></td>
-                    <td><?php echo($clientid); ?></td>
-                </tr>
-                <tr>
-                    <td align="right"><b>Tunnels:</b></td>
-                    <td>
-<?php
-$rstunnels = mysqli_query($dbconn, "SELECT * FROM tunnels") or die("Error in Selecting " . mysqli_error($dbconn));
-$tunnelcount = $rstunnels->num_rows;
-if ($tunnelcount > 0) {
-    while($tunnelrow = mysqli_fetch_assoc($rstunnels)) {
-        echo("                        " . $tunnelrow["tunnelname"] . " (" . $tunnelrow["localhost"] . ":" . $tunnelrow["localport"] . " to " . $tunnelrow["tunnelport"] . ")<br>\n");
-    }
-} else {
-    echo("                        None\n");
-}
-?>                    </td>
-                </tr>
-                <tr>
-                    <td align="right"><b>Tunnel Service:</b></td>
-                    <td><?php echo(ucwords(shell_exec("systemctl is-active callmigrate-tunnel"))); ?></td>
-                </tr>                                
-            </table>
+            <div id="statusholder">
+            </div>
         </div>
         <div class="cm-footer">
             <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
