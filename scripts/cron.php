c22f33c9eccbda51a91a4c637682f9ce146960f3
Adam Blomfield <adam@blomfield.me>
2025-01-07 19:14:23 -0600

    cron

--- scripts/cron.php
+++ scripts/cron.php
@@ -1,22 +1,6 @@
 <?php
-// Import Settings
-$yamlsettings = yaml_parse_file('/opt/callmigrate/settings.yaml');
-$dbserver = $yamlsettings['Database']['ServerName'];
-$dbuser = $yamlsettings['Database']['Username'];
-$dbpass = $yamlsettings['Database']['Password'];
-$dbname = $yamlsettings['Database']['DBName'];
-
-// Load Settings
-$dbconn = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-if ($dbconn->connect_error) {
-  die("Connection failed: " . $dbconn->connect_error);
-}
-$rssettings = mysqli_query($dbconn, "SELECT * FROM settings") or die("Error in Selecting " . mysqli_error($dbconn));
-$rowsettings = mysqli_fetch_assoc($rssettings);
-$regstatus = $rowsettings["regstatus"];
-$clientid = $rowsettings["clientid"];
-$clientsecret = $rowsettings["clientsecret"];
-$cmserver = $rowsettings["cmserver"];
+// Retrieve Settings
+include("/opt/callmigrate/html/includes/settings.php");
 
 // Open Log File
 $logfile = fopen("/opt/callmigrate/logs/callmigrate.log", "a") or die("Unable to open file!");
