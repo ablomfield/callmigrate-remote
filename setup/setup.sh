12c3c235bb5657e0d1adf1d82ff7712f55e945d4
Adam Blomfield <adam@blomfield.me>
2025-01-09 12:57:09 -0600

    fix setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,13 +1,12 @@
-cd ~/callmigrate/setup
-sudo mysql < callmigrate-remote.sql
-mkdir /opt/callmigrate/logs/
+cd /opt/callmigrate/setup
+sudo mysql < /opt/callmigrate/conf/callmigrate-remote.sql
 sudo ln -s /opt/callmigrate/conf/100-callmigrate.conf /etc/apache2/sites-available/100-callmigrate.conf
 sudo a2dissite 000-default
 sudo a2ensite 100-callmigrate
 sudo systemctl reload apache2
-sudo cp ~/callmigrate/conf/callmigrate-tunnel.service /etc/systemd/system
+sudo cp /opt/callmigrate/conf/callmigrate-tunnel.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable callmigrate-tunnel.service
-sudo cp ~/callmigrate/conf/cm-cron /etc/cron.d
-sudo cp ~/callmigrate/conf/cm-logrotate /etc/cron.d
+sudo cp /opt/callmigrate/conf/cm-cron /etc/cron.d
+sudo cp /opt/callmigrate/conf/cm-logrotate /etc/cron.d
 sudo service cron restart
