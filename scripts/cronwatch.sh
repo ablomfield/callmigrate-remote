39cf84b99a869254b9c2f34e5b238d30003c0f42
Adam Blomfield <adam@blomfield.me>
2025-01-11 21:05:14 -0600

    cronwatch

--- scripts/cronwatch.sh
+++ scripts/cronwatch.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-FILE_TO_WATCH="/opt/callmigrate/cronwatch/cron.now"
-
-#!/bin/bash
-
-# Your service logic here
-function log_watch {
-  CURRENT_HASH=$(sha256sum $FILE_TO_WATCH | awk '{print $1}')
-  if [ -z "$LAST_HASH" ]; then
-    LAST_HASH="$CURRENT_HASH"
-  fi
-
-  if [ "$CURRENT_HASH" != "$LAST_HASH" ]; then
-    /bin/php /opt/callmigrate/scripts/cron.php
-    LAST_HASH="$CURRENT_HASH"
-  fi
-
-  sleep 1 # Check every 1 second
-}
-
 while true; do
-  log_watch
+    # Watch for modifications to the file
+    inotifywait -e modify "/opt/callmigrate/cronwatch/cron.now"
+
+    # Run cron.php script
+    echo "Manual cron triggered"
+    /usr/bin/php /opt/callmigrate/scripts/cron.php
 done
